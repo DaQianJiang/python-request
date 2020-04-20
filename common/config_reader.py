2645c921446a5bb81036cd5ebbba769ba8064c97
jiangqian <jiangqian@sm.vvip-u.com>
2020-04-20 17:26:42 +0800

    config配置

---
+++ common/config_reader.py
@@ -0,0 +1,38 @@
+import os
+import yaml
+
+
+ROOT_PATH = os.path.dirname(os.path.abspath('.'))
+CONFIG_PATH = os.path.join(ROOT_PATH,'config\config.yaml')
+
+class configReader():
+
+    #读取配置文件内容
+    def get_configv_value(self,element):
+        if os.path.exists(CONFIG_PATH):
+            with open(CONFIG_PATH) as f :
+                self.config_value = yaml.safe_load(f.read()).get(element)
+            return self.config_value
+        else:
+            raise FileNotFoundError("配置文件不存在")
+
+    #读取url内容
+    def get_url(self):
+        value = self.get_configv_value("base_url")
+        return value
+
+    #读取其它配置的内容
+
+
+if __name__ == '__main__':
+    c = configReader()
+    print(c.get_url())
+
+
+
+
+
+
+
+
+
